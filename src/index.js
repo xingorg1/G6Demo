122e59dcb1cf25bc48100d3af55ad0786fb0d1b7
xing.org1^ <xing.org1@outlook.com>
2021-01-12 17:32:21 +0800

    加「上下文菜单」

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import G6 from "@antv/g6";
-// Background Animation
+// 节点动画
 G6.registerNode(
   "background-animate",
   {
@@ -82,9 +82,43 @@ G6.registerNode(
   },
   "circle"
 );
+// 小地图
 const minimap = new G6.Minimap({
   size: [150, 100]
 });
+// 上下文菜单
+const contextMenu = new G6.Menu({
+  getContent(evt) {
+    let header;
+    if (evt.target && evt.target.isCanvas && evt.target.isCanvas()) {
+      header = "Canvas ContextMenu";
+    } else if (evt.item) {
+      const itemType = evt.item.getType();
+      header = `${itemType.toUpperCase()} ContextMenu`;
+    }
+    return `
+    <h3>${header}</h3>
+    <ul>
+      <li title='1'>li 1</li>
+      <li title='2'>li 2</li>
+      <li>li 3</li>
+      <li>li 4</li>
+      <li>li 5</li>
+    </ul>`;
+  },
+  handleMenuClick: (target, item) => {
+    console.log(target, item);
+  },
+  // offsetX and offsetY include the padding of the parent container
+  // 需要加上父级容器的 padding-left 16 与自身偏移量 10
+  offsetX: 16 + 10,
+  // 需要加上父级容器的 padding-top 24 、画布兄弟元素高度、与自身偏移量 10
+  offsetY: 0,
+  // the types of items that allow the menu show up
+  // 在哪些类型的元素上响应
+  itemTypes: ["node", "edge", "canvas"]
+});
+// 数据
 const data = {
   nodes: [
     {
@@ -152,6 +186,7 @@ const data = {
     }
   ]
 };
+// 图形绘制
 const container = document.getElementById("container");
 const width = container.scrollWidth;
 const height = container.scrollHeight || 660;
@@ -165,7 +200,7 @@ const graph = new G6.Graph({
     // 支持的 behavior
     edit: ["click-select"]
   },
-  plugins: [minimap],
+  plugins: [minimap, contextMenu],
   layout: {
     type: "dagre",
     rankdir: "LR"
